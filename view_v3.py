996bd2b9b24318df51603b20fc59162ebc55333b
dropcity <selmueller42@gmail.com>
2015-11-24 09:52:50 -0600

    changed axes maximum and added title for window, inserted comments

--- view_v3.py
+++ view_v3.py
@@ -4,6 +4,7 @@ plt.ion()
 class GraphAnimation():
 
 	def __init__(self):
+		#create figure and subplots with position 1,2, 3 and 4
 		self.fig = plt.figure()
 		self.x_g1 = 0
 		self.g1 = self.fig.add_subplot(2,2,1)
@@ -14,53 +15,63 @@ class GraphAnimation():
 		self.x_g4 = 0
 		self.g4 = self.fig.add_subplot(2,2,4)
 
+		#sets name for window
+		self.fig.canvas.set_window_title('Muscle Activity')
+
+		#lists for x and y values
 		self.xdata_g1, self.ydata_g1 = [], []
 		self.xdata_g2, self.ydata_g2 = [], []
 		self.xdata_g3, self.ydata_g3 = [], []
 		self.xdata_g4, self.ydata_g4 = [], []
 
+		#plot for axes g1
 		self.line_g1, = self.g1.plot([], [])
-		self.g1.set_xlim(0, 120)
-		self.g1.set_ylim(0, 100)
+		#x and y limit of axes
+		self.g1.set_xlim(0, 60)
+		self.g1.set_ylim(0, 120)
+		#grid for graph
 		self.g1.grid()
 
 		self.line_g2, = self.g2.plot([], [])
-		self.g2.set_xlim(0,120)
-		self.g2.set_ylim(0,100)
+		self.g2.set_xlim(0,60)
+		self.g2.set_ylim(0,120)
 		self.g2.grid()
 
 		self.line_g3, = self.g3.plot([], [])
-		self.g3.set_xlim(0,120)
-		self.g3.set_ylim(0,100)
+		self.g3.set_xlim(0,60)
+		self.g3.set_ylim(0,120)
 		self.g3.grid()
 
 		self.line_g4, = self.g4.plot([], [])
-		self.g4.set_xlim(0,120)
-		self.g4.set_ylim(0,100)
+		self.g4.set_xlim(0,60)
+		self.g4.set_ylim(0,120)
 		self.g4.grid()
 
 	def drawG1(self, new_y):
-		self.x_g1 += 5
+		#draws every second
+		self.x_g1 += 1
+		# append new values to x ynd y lists
 		self.xdata_g1.append(self.x_g1)
 		self.ydata_g1.append(new_y)
 
+		#check if drawn lines exceed x axes,
+		#if so, change x axes range
 		xmin_g1, xmax_g1 = self.g1.get_xlim()
-
 		if self.x_g1 >= xmax_g1:
 			self.g1.set_xlim(xmax_g1, xmax_g1+120)
-
+		#sets the x and y value for line in axes g1
 		self.line_g1.set_data(self.x_g1, new_y)
 
 		#rescale plot
 		self.g1.relim()
 		self.g1.autoscale_view()
-
+		#plot and show line g1 and pause for 1 millisec
 		self.g1.plot(self.xdata_g1, self.ydata_g1, '-k')
 		plt.show()
 		plt.pause(0.1)
 
 	def drawG2(self, new_y):
-		self.x_g2 += 5
+		self.x_g2 += 1
 
 		self.xdata_g2.append(self.x_g2)
 		self.ydata_g2.append(new_y)
@@ -81,7 +92,7 @@ class GraphAnimation():
 		plt.pause(0.1)
 
 	def drawG3(self, new_y):
-		self.x_g3 += 5
+		self.x_g3 += 1
 
 		self.xdata_g3.append(self.x_g3)
 		self.ydata_g3.append(new_y)
@@ -102,7 +113,7 @@ class GraphAnimation():
 		plt.pause(0.1)
 
 	def drawG4(self, new_y):
-		self.x_g4 += 5
+		self.x_g4 += 1
 
 		self.xdata_g4.append(self.x_g4)
 		self.ydata_g4.append(new_y)
